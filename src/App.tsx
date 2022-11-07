28d1ee7861c2509845b017fe3cc7122609a0f670
Raman Davidovich <raman.davidovich@gmail.com>
2022-11-07 19:45:21 +0300

    feat: added routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,17 @@
 import React from 'react';
+import { Routes, Route } from 'react-router-dom';
+import MonthStatistics from './pages/MonthStatistics/MonthStatistics';
+import Home from './pages/Home/Home';
 
 function App() {
-  return <div className="App" />;
+  return (
+    <div className="App">
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/month-statistics" element={<MonthStatistics />} />
+      </Routes>
+    </div>
+  );
 }
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { BrowserRouter as Router } from 'react-router-dom';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import './styles/index.scss';
@@ -8,6 +9,8 @@ const root = ReactDOM.createRoot(
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Router>
+      <App />
+    </Router>
   </React.StrictMode>,
 );
