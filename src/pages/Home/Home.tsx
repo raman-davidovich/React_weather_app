e6a0747f5193f1850c07b9d042a27c6a4385ae8c
Raman Davidovich <raman.davidovich@gmail.com>
2022-11-07 19:17:01 +0300

    feat: created pages

---
+++ src/pages/Home/Home.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from './Home.module.scss';
+
+interface Props {}
+
+function Home(props: Props) {
+  return (
+    <div className={s.home}>Home</div>
+  );
+}
+
+export default Home;
---
+++ src/pages/MonthStatistics/MonthStatistics.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import s from './MonthStatistics.module.scss';
+
+interface Props {}
+
+function MonthStatistics(props: Props) {
+  return (
+    <div className={s.home}>Month statistics</div>
+  );
+}
+
+export default MonthStatistics;
