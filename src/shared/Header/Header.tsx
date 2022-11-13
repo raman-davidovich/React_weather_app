7fb2ac98ac8a85e605279e859a12ad2bdc7908b4
Raman Davidovich <raman.davidovich@gmail.com>
2022-11-13 20:56:30 +0300

    feat: added react-select

--- src/shared/Header/Header.tsx
+++ src/shared/Header/Header.tsx
@@ -1,8 +1,15 @@
 import React from 'react';
+import Select from 'react-select';
 import s from './Header.module.scss';
 import GlobalSvgSelector from '../../assets/images/icons/global/GlobalSvgSelector';
 
 function Header() {
+  const options = [
+    { value: 'chocolate', label: 'Chocolate' },
+    { value: 'strawberry', label: 'Strawberry' },
+    { value: 'vanilla', label: 'Vanilla' },
+  ];
+
   return (
     <div className={s.header}>
       <div className={s.wrapper}>
@@ -10,7 +17,10 @@ function Header() {
         <div className={s.title}>React weather</div>
       </div>
       <div className={s.wrapper}>
-        <div className={s.change_theme}><GlobalSvgSelector id="change-theme" /></div>
+        <div className={s.change_theme}>
+          <GlobalSvgSelector id="change-theme" />
+          <Select options={options} />
+        </div>
       </div>
     </div>
   );
