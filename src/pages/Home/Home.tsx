176c0e80d09ac0dfd7a024ba3d3739e891ea7606
Raman Davidovich <raman.davidovich@gmail.com>
2022-11-14 22:27:42 +0300

    feat: implemented ThisDay component

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,9 +1,12 @@
 import React from 'react';
 import s from './Home.module.scss';
+import ThisDay from './components/ThisDay/ThisDay';
 
 function Home() {
   return (
-    <div className={s.home}>Home</div>
+    <div className={s.home}>
+      <ThisDay />
+    </div>
   );
 }
 
--- src/pages/Home/components/ThisDay/ThisDay.tsx
+++ src/pages/Home/components/ThisDay/ThisDay.tsx
@@ -1,8 +1,28 @@
 import React from 'react';
+import s from './ThisDay.module.scss';
+import GlobalSvgSelector from '../../../../assets/images/icons/global/GlobalSvgSelector';
 
 function ThisDay() {
   return (
-    <div>This day</div>
+    <div className={s.this__day}>
+      <div className={s.top__block}>
+        <div className={s.top__block_wrapper}>
+          <div className={s.this__temp}>20°</div>
+          <div className={s.this__day_name}>Today</div>
+        </div>
+        <GlobalSvgSelector id="sun" />
+      </div>
+      <div className={s.bottom__block}>
+        <div className={s.this__time}>
+          Time:
+          <span> 21:54</span>
+        </div>
+        <div className={s.this__city}>
+          City:
+          <span> Baranovichi</span>
+        </div>
+      </div>
+    </div>
   );
 }
 
