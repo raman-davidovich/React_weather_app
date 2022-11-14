a7f322a485277fc0d57768d1f8829944595dec52
Raman Davidovich <raman.davidovich@gmail.com>
2022-11-14 21:56:04 +0300

    feat: created ThisDay and ThisDayInfo components

---
+++ src/pages/Home/components/ThisDay/ThisDay.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function ThisDay() {
+  return (
+    <div>This day</div>
+  );
+}
+
+export default ThisDay;
---
+++ src/pages/Home/components/ThisDayInfo/ThisDayInfo.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function ThisDayInfo() {
+  return (
+    <div>This day info</div>
+  );
+}
+
+export default ThisDayInfo;
